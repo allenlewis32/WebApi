1f1fbd3119db543e2780dd613d236e121ddbda98
Allen Lewis <allenlewis32@gmail.com>
2023-04-17 10:39:02 +0530

    Add project files.

---
+++ WebApplication4/Controllers/BookController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using WebApplication4.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        IConfiguration _configuration;
+        public BookController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // GET: api/<BookController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<BookModel> books = new();
+            SqlConnection conn = new(_configuration.GetConnectionString("db"));
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new("select * from books", conn);
+                using (var reader = cmd.ExecuteReader()) {
+                    while (reader.Read())
+                    {
+                        BookModel book = new();
+                        book.Id = reader.GetInt32(0);
+                        book.BookCode = reader.GetString(1);
+                        book.BookTitle = reader.GetString(2);
+                        book.Author = reader.GetString(3);
+                        book.Price = reader.GetInt32(4);
+                        book.SupplierId = reader.GetString(5);
+                        books.Add(book);
+                    }
+                }
+            }
+            catch
+            {
+                return NotFound();
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return Ok(books);
+        }
+
+        // GET api/<BookController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BookController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BookController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BookController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication4/Models/BookModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication4.Models
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+        public string BookCode { get; set; }
+        public string BookTitle { get; set; }
+        public string Author { get; set; }
+        public int Price { get; set; }
+        public string SupplierId { get; set; }
+    }
+}
